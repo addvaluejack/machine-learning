bd395c0af79e1230cc16fb45e32f34c1eb9110e5
Muye Jiang <mjian080@uottawa.ca>
2016-09-08 15:33:57 -0400

    Completed ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,34 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+steps = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+%initialize C and sigma
+optimized_C_index = 1;
+optimized_sigma_index = 1;
+model = svmTrain(X, y, steps(optimized_C_index), @(x1, x2) gaussianKernel(x1, x2, steps(optimized_sigma_index)));
+predictions = svmPredict(model, Xval);
+optimized_error = mean(double(predictions ~= yval));
 
+%start the loop to choose optimized C and sigma
+for i = 1:8
+    for j = 1:8
+        if i == 1 && j == 1
+            continue;
+        end
+        model = svmTrain(X, y, steps(i), @(x1, x2) gaussianKernel(x1, x2, steps(j)));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        if error < optimized_error
+            optimized_C_index = i;
+            optimized_sigma_index = j;
+            optimized_error = error;
+        end;
+    end;
+end;
 
-
+C = steps(optimized_C_index);
+sigma = steps(optimized_sigma_index);
 
 
 % =========================================================================
